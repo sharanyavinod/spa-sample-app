297d0c62a8fd3d64b06b0a7c8566d70f689b2ae7
Hanish Bansal <habansal@adobe.com>
2020-10-08 20:34:37 +0530

    feat add support for virtual components

--- src/components/Text.js
+++ src/components/Text.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from '../styles/common.css';
-import { withModel } from '@adobe/aem-react-editable-components';
+import { WrapAsAEMEditableComponent } from '@adobe/aem-react-editable-components';
 
 export const TextEditConfig = {
     emptyLabel: 'Text',
     isEmpty: function(props) {
         return !props || !props.text || props.text.trim().length < 1;
-    }
+    },
+    aemResourceType: "we-retail-journal/components/text"
 };
 
 const Text = ({ cqPath, richText, text }) => {
@@ -14,9 +15,8 @@ const Text = ({ cqPath, richText, text }) => {
         <div className={classes.aem_text} id={cqPath.substr(cqPath.lastIndexOf('/') + 1)} data-rte-editelement dangerouslySetInnerHTML={{__html: text}}/>
     );
     return richText ? richTextContent() : (<div className={classes.aem_text}>{text}</div>);
-}
+};
 
 export default Text;
 
-
-export const AEMText =  withModel(Text, { injectPropsOnInit: true });
+export const AEMText = WrapAsAEMEditableComponent(Text, TextEditConfig);
