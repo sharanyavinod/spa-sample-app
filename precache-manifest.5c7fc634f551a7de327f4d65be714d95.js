747ffec61e569b9fda184e9fc44a98aa259c11bf
sharanyavinod <sharanyavinod90@gmail.com>
2020-11-18 18:27:31 +0100

    Updates

---
+++ precache-manifest.5c7fc634f551a7de327f4d65be714d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcd0b36ff45daa8934c2fb99617da10a",
+    "url": "/spa-sample-app/index.html"
+  },
+  {
+    "revision": "09ddae38bc9f15d644b9",
+    "url": "/spa-sample-app/static/css/main.6a841639.chunk.css"
+  },
+  {
+    "revision": "e2e658f77e4d08472450",
+    "url": "/spa-sample-app/static/js/2.3b02794c.chunk.js"
+  },
+  {
+    "revision": "718dc7d4dcb33bab6f416ce3f100dbe6",
+    "url": "/spa-sample-app/static/js/2.3b02794c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09ddae38bc9f15d644b9",
+    "url": "/spa-sample-app/static/js/main.df65082d.chunk.js"
+  },
+  {
+    "revision": "a59c45281576dd2b5952",
+    "url": "/spa-sample-app/static/js/runtime-main.3ecbce96.js"
+  }
+]);
